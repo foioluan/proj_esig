a0373c9e2d61fbe55374db06ea0ae6d593066b74
foioluan <luan.vfv@gmail.com>
2025-02-14 10:24:23 -0300

    pre final updates

--- target/classes/META-INF/sql/initial-data.sql
+++ target/classes/META-INF/sql/initial-data.sql
@@ -1,4 +1,1 @@
 INSERT INTO user_table (email, password, name, isAdmin) VALUES ('luan.vfv@gmail.com', '2005', 'Luan', true);
-INSERT INTO user_table (email, password, name, isAdmin) VALUES ('luan@gmail.com', '2005', 'Luan 2', false);
-INSERT INTO task_table (title, description, priority, status, deadline, user_id) VALUES ('TESTE 1', 'DESC TESTE 1', 'High', 'In Progress', '2025-2-15', 2);
-INSERT INTO task_table (title, description, priority, status, deadline, user_id) VALUES ('TESTE 2', 'DESC TESTE 2', 'High', 'In Progress', '2025-2-15', 2);
